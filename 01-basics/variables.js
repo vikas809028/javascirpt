1de5e33c28fb3398402ea082596e1ec1acc30169
Vikas Tiwari <114413363+vikas809028@users.noreply.github.com>
2024-02-15 11:26:58 +0000

    updated

---
+++ 01-basics/variables.js
@@ -0,0 +1,16 @@
+const accoundId = 123 // can not be changed
+let name = "Vikas Tiwari"
+var city = "Lakhimpur kheri" // ver is not used because it suffer from block level problem
+
+pincode1 = "2627222"
+pincode2 = 262722
+
+let pincode ;
+
+
+console.table([accoundId,name,city,pincode])
+
+console.log(typeof(name))
+console.log(typeof(pincode1))
+console.log(typeof(pincode2))
+console.log(typeof(pincode))
